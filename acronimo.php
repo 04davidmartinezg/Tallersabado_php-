bbf54665d901267d2c7eb96d38d723c3787c1a27
04davidmartinezg <ndmartinezg@uniboyaca.edu.co>
2026-03-28 10:49:34 -0500

    acronimo listo

---
+++ acronimo.php
@@ -0,0 +1,47 @@
+<?php
+class GeneradorAcronimo {
+    private $frase = null;
+
+    function __construct($texto) {
+        $this->frase = $texto;
+    }
+    public function obtenerAcronimo() {
+        $reemplazar = str_replace('-', ' ', $this->frase);
+        $soloLetras = preg_replace('/[^\w\s]/', '', $reemplazar);
+        $palabras = explode(' ', mb_strtoupper($soloLetras));
+        
+        $acronimo = "";
+        foreach ($palabras as $palabrita) {
+            if (!empty($palabrita)) {
+                $acronimo .= $palabrita[0]; 
+            }
+        }
+        return $acronimo;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Generador de Acrónimos</title>
+    <link rel="stylesheet" href="estilos.css">
+    <body>
+        <div class="contenedor">
+        <h1>Genera tu acronimo</h1>
+        <form method="POST">
+            <input type="text" name="frase" required>
+            <button type="Submit">Convertir</button>
+        </form>
+        <?php
+        if ($_POST && isset($_POST['frase'])) {
+            require_once 'acronimo.php';
+            $objeto = new GeneradorAcronimo($_POST['frase']);
+            echo "<div class='resultado'>Resultado: <strong>" . $objeto->obtenerAcronimo() . "</strong></div>";
+        }
+        ?>
+        </div>
+    </body>
+</head>
+</html>
